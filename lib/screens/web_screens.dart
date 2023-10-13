9c18d72b164f8c1fa1e1200b750db98184d0f8df
rostikKalabishka <rostik31052002@gmail.com>
2023-10-13 19:58:07 +0300

    Displaying Web Chat Input

--- lib/screens/web_screens.dart
+++ lib/screens/web_screens.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_responsive_ui/utils/colors.dart';
 import 'package:whatsapp_responsive_ui/widgets/contacts.dart';
 
 import '../widgets/chat_list.dart';
@@ -30,13 +31,73 @@ class WebScreenLayout extends StatelessWidget {
                 ),
                 fit: BoxFit.cover),
           ),
-          child: const Column(children: [
-            WebChatAppBar(),
-            SizedBox(height: 20),
-            Expanded(
-              child: ChatList(),
-            ),
-          ]),
+          child: Column(
+            children: [
+              const WebChatAppBar(),
+              const SizedBox(height: 20),
+              const Expanded(
+                child: ChatList(),
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height * 0.07,
+                padding: const EdgeInsets.all(10),
+                decoration: const BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(color: dividerColor),
+                  ),
+                  color: chatBarMessage,
+                ),
+                child: Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.emoji_emotions_outlined,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.attach_file,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                          left: 10,
+                          right: 15,
+                        ),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: searchBarColor,
+                            hintText: 'Type a message',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20.0),
+                              borderSide: const BorderSide(
+                                width: 0,
+                                style: BorderStyle.none,
+                              ),
+                            ),
+                            contentPadding: const EdgeInsets.only(left: 20),
+                          ),
+                        ),
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.mic,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ]),
     );
