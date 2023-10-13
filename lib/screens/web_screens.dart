8832a996da4fcf19590fd97f19a3ca55ed9543f2
rostikKalabishka <rostik31052002@gmail.com>
2023-10-13 17:45:53 +0300

    Displaying Web Contacts List

--- lib/screens/web_screens.dart
+++ lib/screens/web_screens.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_responsive_ui/widgets/contacts.dart';
 
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Web')),
+    return Scaffold(
+      body: Row(children: [
+        const Expanded(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                //web app bar
+                //web search
+                Contacts()
+              ],
+            ),
+          ),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width * 0.75,
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage(
+                    'backgroundImage.png',
+                  ),
+                  fit: BoxFit.cover)),
+        ),
+      ]),
     );
   }
 }
--- lib/widgets/contacts.dart
+++ lib/widgets/contacts.dart
@@ -10,45 +10,44 @@ class Contacts extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 10),
-      child: Column(
-        children: [
-          Expanded(
-            child: ListView.separated(
-              shrinkWrap: true,
-              itemBuilder: (context, index) {
-                return InkWell(
-                  onTap: () {
-                    return goToDialog(index, context, '${info[index]['name']}');
-                  },
-                  child: ListTile(
-                    title: Text('${info[index]['name']}',
-                        style: const TextStyle(fontSize: 18)),
-                    leading: CircleAvatar(
-                      backgroundImage: NetworkImage(
-                        '${info[index]['profilePic']}',
-                      ),
-                    ),
-                    subtitle: Padding(
-                      padding: const EdgeInsets.only(top: 6),
-                      child: Text('${info[index]['message']}',
-                          style: const TextStyle(fontSize: 15)),
-                    ),
-                    trailing: Text(
-                      '${info[index]['time']}',
-                      style: const TextStyle(fontSize: 13, color: Colors.grey),
+      child: ListView.separated(
+        shrinkWrap: true,
+        itemBuilder: (context, index) {
+          return Column(
+            children: [
+              InkWell(
+                onTap: () {
+                  // return goToDialog(index, context, '${info[index]['name']}');
+                },
+                child: ListTile(
+                  title: Text('${info[index]['name']}',
+                      style: const TextStyle(fontSize: 18)),
+                  leading: CircleAvatar(
+                    radius: 30,
+                    backgroundImage: NetworkImage(
+                      '${info[index]['profilePic']}',
                     ),
                   ),
-                );
-              },
-              itemCount: info.length,
-              separatorBuilder: (BuildContext context, int index) {
-                return const Divider(
-                  color: dividerColor,
-                );
-              },
-            ),
-          ),
-        ],
+                  subtitle: Padding(
+                    padding: const EdgeInsets.only(top: 6),
+                    child: Text('${info[index]['message']}',
+                        style: const TextStyle(fontSize: 15)),
+                  ),
+                  trailing: Text(
+                    '${info[index]['time']}',
+                    style: const TextStyle(fontSize: 13, color: Colors.grey),
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
+        itemCount: info.length,
+        separatorBuilder: (BuildContext context, int index) {
+          return const Divider(
+            color: dividerColor,
+          );
+        },
       ),
     );
   }
